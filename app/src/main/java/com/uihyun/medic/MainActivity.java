8b70f5229c192fcb11c59b88a82c8306ba67df0d
uihyun <uihyunkei@gmail.com>
2017-01-18 23:49:21 +0900

    google analytics tracker 변경. 버전 업.

--- app/src/main/java/com/uihyun/medic/MainActivity.java
+++ app/src/main/java/com/uihyun/medic/MainActivity.java
@@ -124,6 +124,7 @@ public class MainActivity extends Activity {
 
         Tracker t = AppController.getInstance().getTracker(AppController.TrackerName.APP_TRACKER);
         t.setScreenName("MainActivity");
+        t.enableAdvertisingIdCollection(true);
         t.send(new HitBuilders.AppViewBuilder().build());
     }
 
